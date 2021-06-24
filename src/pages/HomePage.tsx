b0b7ed38eb22d636b9c6f3dafb2ed1e1e4938d3c
Pedro Fritsch <pedrofritsch98@gmail.com>
2021-06-24 19:12:57 -0300

    ícones corretos

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,37 @@
 import React, { useCallback, useMemo } from 'react'
-import { WiNightAltRain } from "react-icons/wi";
+import { WiCloud, WiCloudy, WiDayCloudy, WiDayShowers, WiDaySunny, WiNightClear, WiNightCloudy, WiNightShowers, WiRainMix, WiSnow, WiThunderstorm } from "react-icons/wi";
 import { MdPlace } from "react-icons/md";
 import { useEffect } from 'react';
 import { useState } from 'react';
 import useWeather, { IReturn } from '../hooks/useWeather';
 import styled from '@emotion/styled';
 import { Flex } from '@chakra-ui/react';
+
+
+const weatherToIcon: {
+  iconName: string;
+  icon: JSX.Element;
+}[] = [
+  { iconName: "01d", icon: <WiDaySunny /> },
+  { iconName: "02d", icon: <WiDayCloudy /> },
+  { iconName: "03d", icon: <WiCloud /> },
+  { iconName: "04d", icon: <WiCloudy /> },
+  { iconName: "09d", icon: <WiRainMix /> },
+  { iconName: "10d", icon: <WiDayShowers /> },
+  { iconName: "11d", icon: <WiThunderstorm /> },
+  { iconName: "13d", icon: <WiSnow /> },
+  { iconName: "50d", icon: <WiDaySunny /> },
+  
+  { iconName: "01n", icon: <WiNightClear /> },
+  { iconName: "02n", icon: <WiNightCloudy /> },
+  { iconName: "03n", icon: <WiCloud /> },
+  { iconName: "04n", icon: <WiCloudy /> },
+  { iconName: "09n", icon: <WiRainMix /> },
+  { iconName: "10n", icon: <WiNightShowers /> },
+  { iconName: "11n", icon: <WiThunderstorm /> },
+  { iconName: "13n", icon: <WiSnow /> },
+  { iconName: "50n", icon: <WiDaySunny /> },
+]
 
 const Container = styled.div`
   width: 100vw;
@@ -34,11 +60,13 @@ function HomePage() {
   const city = useMemo<string>(() => 'novo hamburgo', [])
 
   const [ weather, setWeather ] = useState<IReturn>()
+  const [ weatherIcon, setWeatherIcon ] = useState<string>()
   const [ temperature, setTemperature ] = useState<number>()
   const [ cityName, setCityName ] = useState<string>()
   
   useEffect(() => {
     console.log({ weather })
+    setWeatherIcon(weather?.weather[0].icon)
     setTemperature(weather?.main.temp)
     setCityName(`${weather?.name} - ${weather?.sys.country}`)
   }, [weather])
@@ -61,7 +89,9 @@ function HomePage() {
     <Container>
       {weather &&
         <>
-          <WiNightAltRain fontSize='10em' color='white'/>
+          <Flex fontSize='10em' color='white'>
+            {weatherToIcon.find(w => w.iconName === weatherIcon)?.icon}
+          </Flex>
 
           <Temperature>{temperature}ºc</Temperature>
           <Flex alignItems='center' color='white' gridGap='5px'>
