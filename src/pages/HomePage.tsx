75f4dc7e39f3f10d10f4ef71aa0888f439ca53ca
Pedro Fritsch <pedrofritsch98@gmail.com>
2021-06-24 20:24:03 -0300

    adicionado pesquisa de cidade

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import useWeather, { IReturn } from '../hooks/useWeather';
 import styled from '@emotion/styled';
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Input } from '@chakra-ui/react';
 import { css } from '@emotion/react';
 
 
@@ -61,14 +61,23 @@ function HomePage() {
   const { getWeatherByCityName } = useWeather()
 
   const apiKey = useMemo<string>(() => '427dfea58a376addcf4c40de09f83f45', [])
-  const city = useMemo<string>(() => 'dubai', [])
-
+  
+  const [ city, setCity ] = useState<string>('')
+  const [ searchCity, setSearchCity ] = useState<string>('dubai')
   const [ weather, setWeather ] = useState<IReturn>()
   const [ weatherIcon, setWeatherIcon ] = useState<string>()
   const [ temperature, setTemperature ] = useState<number>()
   const [ cityName, setCityName ] = useState<string>()
   const [ background, setBackground ] = useState<string>('#232394')
   
+  const handleChangeCity = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setCity(e.target.value)
+    const timeout = setTimeout(() => {
+      setSearchCity(e.target.value)
+    }, 1000)
+    return () => clearTimeout(timeout)
+  }, [])
+
   useEffect(() => {
     console.log({ weather })
     setWeatherIcon(weather?.weather[0].icon)
@@ -143,12 +152,12 @@ function HomePage() {
   const attWeather = useCallback(async () => {
     try {
       // console.log(attWeather)
-      const weather = await getWeatherByCityName(city, apiKey)
+      const weather = await getWeatherByCityName(searchCity, apiKey)
       setWeather(weather)
     } catch (e) {
       console.error(e)
     }
-  }, [getWeatherByCityName, city, apiKey])
+  }, [getWeatherByCityName, searchCity, apiKey])
 
   useEffect(() => {
     attWeather()
@@ -164,6 +173,18 @@ function HomePage() {
         height='100%'
         background='linear-gradient(0deg, rgba(0, 0, 0, 0) 0%, rgba(255,255,255,0.15) 100%)'
         pointerEvents='none'
+      />
+      <Input
+        variant="flushed"
+        placeholder="Search any city you want"
+        size="lg"
+        top='0'
+        position='absolute'
+        fontSize='30px'
+        height='auto'
+        padding='10px'
+        value={city}
+        onChange={handleChangeCity}
       />
       {weather &&
         <>
